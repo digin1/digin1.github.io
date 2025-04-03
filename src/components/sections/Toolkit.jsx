820c0c6cdd98600dc1219c8ac08143a69d6a37f8
digin <digin@mvm-lin-0045.mvm.ed.ac.uk>
2025-04-03 01:13:56 +0100

    updated toolstack

--- src/components/sections/Toolkit.jsx
+++ src/components/sections/Toolkit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faReact, faJs, faNodeJs, faHtml5, faCss3Alt, faPython, 
